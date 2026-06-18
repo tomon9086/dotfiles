1384e9d7e57bfd69f4d48e8b00d230ca1a8066db
tomon9086 <tomon9086@gmail.com>
2026-06-18 11:54:21 +0900

    feat(history): 履歴から選択したコマンドをプロンプトに注入する機能を追加

--- zsh/function/common.zsh
+++ zsh/function/common.zsh
@@ -1,14 +1,26 @@
+typeset -g _HISTORY_SELECT_CMD=""
+
 function history() {
   if [[ "$#" == "0" ]]; then
     local cmd
     cmd=$(fc -ln 1 | fzy)
     if [[ -n "$cmd" ]]; then
-      print -z "$cmd"
+      _HISTORY_SELECT_CMD="$cmd"
     fi
   else
     builtin history "$@"
   fi
 }
+
+# ZLE が端末を初期化した後に BUFFER へ注入する（ZLE 外で fzy を使うと編集不可になる問題の回避）
+function _history_inject_zle() {
+  if [[ -n "$_HISTORY_SELECT_CMD" ]]; then
+    BUFFER="$_HISTORY_SELECT_CMD"
+    CURSOR=$#BUFFER
+    _HISTORY_SELECT_CMD=""
+  fi
+}
+zle -N zle-line-init _history_inject_zle
 
 function gibo() {
 	if [ -z "$2" ] &&  [ "dump" = "$1" ]; then
